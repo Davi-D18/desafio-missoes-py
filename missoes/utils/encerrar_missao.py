ba7fa13fc657df879ad214780aa9b6b87a18ab39
Davi-D18 <luiz.araujo@alunomicrolins.com.br>
2025-02-12 19:08:01 -0300

    melhorada a função de encerrar missão para tratar opções inválidas e adicionar limpeza do terminal

--- missoes/utils/encerrar_missao.py
+++ missoes/utils/encerrar_missao.py
@@ -1,13 +1,19 @@
 from .esperar import esperar
+from .limpar_terminal import limpar
 
 
 def encerrar_missao():
   print("")
+  limpar()
   decisao = input("Deseja encerrar esta missão? (s/n) ").strip().lower()
       
-  if decisao != "n":
+  if decisao == "s":
     print("Saindo da missão...")
     esperar(1)
     return True
-  else:
+  elif decisao == "n":
     return False
+  else:
+    print("Opção inválida, tente novamente.")
+    esperar(1)
+    return encerrar_missao()
